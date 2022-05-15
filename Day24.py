cf33e1040699145b1535410515e5df71f94167d6
iupreti <upretyishwor71@gmail.com>
2022-05-15 14:54:03 -0400

    Day 24 solved

---
+++ Day24.py
@@ -0,0 +1,20 @@
+"""
+Day 24: Average Calories
+Create a function called average_calories that calculates the average calories intake
+of a user. The function should ask the user to input their calories intake for any 
+number of days and once they hit ‘done’ it should calculate and return the average 
+intake.
+"""
+
+
+def average_calories():
+    calories = []
+    while (user_input := input("enter a calories intake when complete enter done: "))!='done':
+        try:
+            calories.append(float(user_input))
+        except ValueError:
+            print('Please enter a valid calories value')
+    print(calories)
+    return f'{sum(calories)/len(calories):.2f}'
+
+print(average_calories())
