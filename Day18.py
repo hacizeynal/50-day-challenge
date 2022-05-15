3ce4e0c8d5293705795aadfb35a085afcb5e783f
iupreti <upretyishwor71@gmail.com>
2022-05-15 07:27:50 -0400

    Day 18 solved

---
+++ Day18.py
@@ -0,0 +1,14 @@
+"""Day 18: Any Number of Arguments
+Write a function called any_number that can receive any number of arguments(integers 
+and floats) and return the average of those integers. If you pass 12, 90, 12, 34 as 
+arguments your function should return 37.0 as average. If you pass 12, 90 your 
+function should return 51.0 as average.
+"""
+
+
+def any_number(*numbers):
+    return sum(numbers)/len(numbers)
+
+
+print(any_number(12, 90, 12, 34))
+print(any_number(12, 90))
