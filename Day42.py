9e98861f142bd1992f8dddaa55f668ca0db709a7
iupreti <upretyishwor71@gmail.com>
2022-05-20 21:50:24 -0400

    git headear updated

--- Day42.py
+++ Day42.py
@@ -1,5 +1,13 @@
+'''
+Day 42: Spelling Checker
+Write a function called spelling_checker. This code asks the user to input a word and
+if a user inputs a wrong spelling it should suggest the correct spelling by asking 
+the user if they meant to type that word. If the user says no, it should ask the 
+user to enter the word again. If the user says yes, it should return the correct 
+word. If the word entered by the user is correctly spelled the function should return
+the correct word. Use the module textblob.
 from textblob import Word
-
+'''
 
 # step 0 make a function called spelling_checker
 def spelling_checker():
