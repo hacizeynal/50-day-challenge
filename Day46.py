02bacfa12afe61600451beca052db1a8e02463f3
iupreti <upretyishwor71@gmail.com>
2022-05-21 08:24:17 -0400

    git day46 solution solved

---
+++ Day46.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+data_frame = pd.DataFrame({'Year': [2009, 2002, 2009, 2010, 2009], 
+            'Title': ['Brother', 'Spider-man', 'WatchMen', 'Inception', 'Avatar'],
+            'Genre': ['Drama', 'Sci-fi', 'Drama', 'Sci-fi', 'Fantasy']})
+print(data_frame)
